04d2df11a471bd74df19aab903f9e5ba42a4a636
Matti ur Rehman <matti.mansha@gmail.com>
2020-11-16 14:21:35 +0500

    Stucture of code is created

---
+++ BST.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node* right;
+    node* left;
+};
+
+class BST
+{
+private:
+    
+public:
+
+};
+
+int main()
+{
+    
+}
